7717114c47b2d56f5054873746d0f5c5308dc9a2
Lizardo <jose.212002lizardo@gmail.com>
2026-03-20 22:08:50 -0500

    Revert to http:// stream URL

--- lib/main.dart
+++ lib/main.dart
@@ -111,8 +111,8 @@ class _SkreenPageState extends State<SkreenPage> {
       debugPrint('[SkreenApp] setProperty error: $e');
     }
 
-    debugPrint('[SkreenApp] Abriendo stream tcp://localhost:9002 ...');
-    await _player.open(Media('tcp://localhost:9002'));
+    debugPrint('[SkreenApp] Abriendo stream http://localhost:9002 ...');
+    await _player.open(Media('http://localhost:9002'));
     debugPrint('[SkreenApp] player.open() completado');
   }
 
